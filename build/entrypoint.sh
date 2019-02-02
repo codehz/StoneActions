de5d8fda0706e5cd1504ad635e95c7b80e9e0494
Code Hz <CodeHz@outlook.com>
2019-02-02 21:39:44 +0800

    fix git submodule

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
+git submodule update --init
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=../install -DCMAKE_BUILD_TYPE=Release -DUSE_COTIRE=OFF .. || (cat CMakeFiles/CMakeOutput.log && cat CMakeFiles/CMakeError.log && exit 1)
