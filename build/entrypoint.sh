05621110b7f2071476d67867830dffb86073966d
Code Hz <CodeHz@outlook.com>
2019-02-02 21:20:34 +0800

    initial commit

---
+++ build/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -ex
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=../install -DCMAKE_BUILD_TYPE=Release -DUSE_COTIRE=OFF .. || (cat CMakeFiles/CMakeOutput.log && cat CMakeFiles/CMakeError.log && exit 1)
+make
+make install
+
