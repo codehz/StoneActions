7b4fdc8c601dd264c0dfd9c7b8126c62bfd6e6be
Code Hz <CodeHz@outlook.com>
2019-02-02 21:44:51 +0800

    fix recursive submodules

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-git submodule update --init
+git submodule update --init --recursive
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=../install -DCMAKE_BUILD_TYPE=Release -DUSE_COTIRE=OFF .. || (cat CMakeFiles/CMakeOutput.log && cat CMakeFiles/CMakeError.log && exit 1)
