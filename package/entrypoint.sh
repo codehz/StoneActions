f806437102741f0c1042428b742115c777052dd0
Code Hz <CodeHz@outlook.com>
2019-02-06 16:00:06 +0800

    fix rootfs issues

--- package/entrypoint.sh
+++ package/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -ex
-mkdir -p image/{usr/lib32,lib,run,tmp,data,game}
+mkdir -p image/{usr/lib32,lib,run/{data,game},tmp}
 cd image
 cat ../lib32.txt | xargs -I {} cp -Lrv /usr/lib32/{} usr/lib32/{}
 cp -Lrv /usr/lib32/ld-linux.so.2 lib
-cp -Prv ../install/bin/* .
+cp -Prv ../install/bin/* run
 docker build -t codehz/stoneserver:latest --no-cache -f - . <<EOF
 FROM scratch
 ADD . /
-VOLUME ["/data","/game","/run"]
+VOLUME ["/run/data","/run/game","/tmp"]
 ENV APID unix:/run/apid.socket
 ENTRYPOINT ["/stone"]
 EOF
