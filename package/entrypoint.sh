7e12c8c0bdde6f4d52ab74d8348de46f38e48ae7
Code Hz <CodeHz@outlook.com>
2019-02-06 20:20:45 +0800

    fix dns again

--- package/entrypoint.sh
+++ package/entrypoint.sh
@@ -4,7 +4,7 @@ cd image
 cat ../lib32.txt | xargs -I {} cp -Lrv /usr/lib32/{} usr/lib32/{}
 cp -Lrv /usr/lib32/ld-linux.so.2 lib
 cp -Prv ../install/bin/* run
-echo "hosts: files mymachines myhostname resolve [!UNAVAIL=return] dns">etc/nsswitch.conf
+echo "hosts: files dns">etc/nsswitch.conf
 docker build -t codehz/stoneserver:latest --no-cache -f - . <<EOF
 FROM scratch
 ADD . /
