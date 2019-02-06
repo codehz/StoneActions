b8d37fdb9608370c11591218c98ac426a4061ffc
Code Hz <CodeHz@outlook.com>
2019-02-06 15:13:53 +0800

    fix docker image

--- package/entrypoint.sh
+++ package/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -ex
-mkdir -p image/{usr/lib32,lib,run}
+mkdir -p image/{usr/lib32,lib,run,tmp,data,game}
 cd image
 cat ../lib32.txt | xargs -I {} cp -Lrv /usr/lib32/{} usr/lib32/{}
 cp -Lrv /usr/lib32/ld-linux.so.2 lib
-cp -Prv ../install/bin/* run
+cp -Prv ../install/bin/* /
 docker build -t codehz/stoneserver:latest --no-cache -f - . <<EOF
 FROM scratch
 ADD . /
-WORKDIR /run
-ENTRYPOINT ["/run/stone"]
+VOLUME ["/data","/game","/run"]
+ENV APID unix:/run/apid.socket
+ENTRYPOINT ["/stone"]
 EOF
 
