979f73d5baed36f3f8abca1c4ab197b319707589
Code Hz <CodeHz@outlook.com>
2019-02-06 15:32:22 +0800

    fix package

--- package/entrypoint.sh
+++ package/entrypoint.sh
@@ -3,7 +3,7 @@ mkdir -p image/{usr/lib32,lib,run,tmp,data,game}
 cd image
 cat ../lib32.txt | xargs -I {} cp -Lrv /usr/lib32/{} usr/lib32/{}
 cp -Lrv /usr/lib32/ld-linux.so.2 lib
-cp -Prv ../install/bin/* /
+cp -Prv ../install/bin/* .
 docker build -t codehz/stoneserver:latest --no-cache -f - . <<EOF
 FROM scratch
 ADD . /
