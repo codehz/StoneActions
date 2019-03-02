8fc16f17f885fb78d13236121cd7b5c35dcbe6d8
Code Hz <codehz@users.noreply.github.com>
2019-03-03 00:52:30 +0800

    Choose debug

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -2,7 +2,7 @@
 git submodule update --init --recursive
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=../install -DCMAKE_BUILD_TYPE=Release -DUSE_COTIRE=OFF .. || (cat CMakeFiles/CMakeOutput.log && cat CMakeFiles/CMakeError.log && exit 1)
+cmake -DCMAKE_INSTALL_PREFIX:PATH=../install -DCMAKE_BUILD_TYPE=Debug -DUSE_COTIRE=OFF .. || (cat CMakeFiles/CMakeOutput.log && cat CMakeFiles/CMakeError.log && exit 1)
 make
 make install
 
